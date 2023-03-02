323859928832bff98c3ea89926580e92bb41c7a8
Skylark92 <skylark92@live.co.kr>
2023-03-02 21:39:57 +0900

    [MarkUp] 댓글 모달 마크업

---
+++ src/components/Comment/Comment.jsx
@@ -0,0 +1,9 @@
+export default function Comment() {
+  return (
+    <div>
+      이미지
+      <strong>익명1413</strong>
+      <button type="button">좋아요</button>
+    </div>
+  )
+}
---
+++ src/components/CommentModal/CommentModal.jsx
@@ -0,0 +1,11 @@
+import Comment from "../Comment/Comment";
+
+export default function CommentModal() {
+  return (
+    <div>
+      <h2>댓글</h2>
+      <hr />
+      <Comment />
+    </div>
+  )
+}
