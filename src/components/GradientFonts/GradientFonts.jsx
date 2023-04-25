6398097376bb2b46c7faa766a4969c5fe6705041
Skylark92 <skylark92@live.co.kr>
2023-04-25 14:37:02 +0900

    [Style] 그라데이션 폰트 및 외곽선 폰트 추가

---
+++ src/components/GradientFonts/GradientFonts.jsx
@@ -0,0 +1,11 @@
+import "./GradientFonts.css";
+
+function GradientFonts({ text }) {
+  return (
+    <span className="gradient-fonts" data-text={text} >
+      {/* {text} */}
+    </span >
+  )
+}
+
+export default GradientFonts;
---
+++ src/components/StrokeFonts/StrokeFonts.jsx
@@ -0,0 +1,11 @@
+import "./StrokeFonts.css";
+
+function StrokeFonts({ text }) {
+  return (
+    <span className="stroke-fonts" data-text={text}>
+      {/* {text} */}
+    </span>
+  )
+}
+
+export default StrokeFonts
