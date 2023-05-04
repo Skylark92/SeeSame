4ccb304bb85ec3adc8db79c5dca647a4c688374b
Skylark92 <skylark92@live.co.kr>
2023-05-04 15:03:44 +0900

    [Feat] 유저 전역 상태 및 리듀서 추가

---
+++ src/store/authSlice.js
@@ -0,0 +1,31 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const authSlice = createSlice({
+  name: "auth",
+  initialState: {
+    isLogin: false,
+    user: null
+  },
+  reducers: {
+    LOGIN(state, action) {
+      localStorage.setItem("seesame-t", action.payload.uid);
+      state.isLogin = true;
+      state.user = {
+        ...action.payload
+      }
+    },
+    LOGOUT(state) {
+      localStorage.removeItem("seesame-t");
+      state.isLogin = false;
+      state.user = null;
+    },
+    UPDATE(state, action) {
+      state.user = {
+        ...action.payload
+      }
+    }
+  }
+});
+
+export const { LOGIN, LOGOUT } = authSlice.actions;
+export default authSlice.reducer;
