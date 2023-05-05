d9003210b9d6bd03e75b6351a07561d84bc601d2
Skylark92 <skylark92@live.co.kr>
2023-05-05 21:02:17 +0900

    [Add] 스토어 구독 추가 및 앱 화면 기본 스타일 설정

--- src/App.js
+++ src/App.js
@@ -1,7 +1,27 @@
+import "./App.css";
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import Router from "./router/Router";
+import getUserData from "./firebase/getUserData";
+import { LOGIN } from "./store/authSlice";
+
 function App() {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    const token = localStorage.getItem("seesame-t");
+
+    if (token) {
+      getUserData(token)
+        .then((res) => {
+          dispatch(LOGIN(res));
+        });
+    }
+  }, [])
+
   return (
-    <div>
-      hello world
+    <div className="app-container">
+      <Router />
     </div>
   );
 }
