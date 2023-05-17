f0246a41247c34e383daa70efbb60d40392d9fbe
Skylark92 <skylark92@live.co.kr>
2023-05-18 01:43:09 +0900

    [Feat] Header 컴포넌트 수정, 뒤로가기 버튼 추가

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,10 @@
 import "./Header.css";
 import GradientFonts from "../GradientFonts/GradientFonts";
 
-function Header({ text }) {
+function Header({ text, backButton, onClick }) {
   return (
     <h2 className="header-title">
+      {backButton && <button className="header-back-button" type="button" onClick={onClick} />}
       <GradientFonts text={text} />
     </h2>
   )
