c00845a8fae6db0abec0ec257e759d37dbefed24
Skylark92 <skylark92@live.co.kr>
2023-05-22 02:14:27 +0900

    [Feat] 공용 버튼 컴포넌트 padding, margin 속성 삭제 / name 속성 추가

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,13 +1,14 @@
 import "./Button.css";
 
-function Button({ size, background, padding, margin, disabled, text, onClick }) {
+function Button({ size, name, background, disabled, text, onClick }) {
   const sizes = ["xSmall", "small", "medium", "large"];
 
   if (!sizes.includes(size)) return null;
 
   return (
     <button className={"common-button " + size}
-      style={{ background, padding, margin }}
+      name={name}
+      style={{ background }}
       onClick={onClick}
       disabled={disabled}
       type="button">
