fee905f34429639251df33bedf65a4e041a412ef
Skylark92 <skylark92@live.co.kr>
2023-05-22 01:59:58 +0900

    [Feat] Header 버튼 onClick 속성 추가

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,10 +1,17 @@
 import "./Header.css";
+import { useNavigate } from "react-router-dom";
 import GradientFonts from "../GradientFonts/GradientFonts";
 
 function Header({ text, backButton, onClick }) {
+  const navigate = useNavigate();
+
+  const defaultHandler = () => {
+    navigate(-1);
+  }
+
   return (
     <h2 className="header-title">
-      {backButton && <button className="header-back-button" type="button" onClick={onClick} />}
+      {backButton && <button className="header-back-button" type="button" onClick={onClick || defaultHandler} />}
       <GradientFonts text={text} />
     </h2>
   )
