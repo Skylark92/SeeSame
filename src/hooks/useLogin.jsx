913f587db2c8377ce2f0d0a0798b8cf12179828b
Skylark92 <skylark92@live.co.kr>
2023-05-22 03:06:01 +0900

    [Feat] useLogin 훅 사용 시 프로필 등록이 안 됐을 때의 분기점 추가

--- src/hooks/useLogin.jsx
+++ src/hooks/useLogin.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react"
 import { useDispatch } from "react-redux";
+import { useNavigate } from "react-router-dom";
 import { LOGIN } from "../store/authSlice";
 import login from "../firebase/login";
 
@@ -8,6 +9,7 @@ function useLogin() {
   const [isPending, setIsPending] = useState(false);
   const [error, setError] = useState(null);
   const dispatch = useDispatch();
+  const navigate = useNavigate();
 
   const userLogin = async (id, password) => {
     setIsPending(true); // 통신 시작
@@ -19,6 +21,8 @@ function useLogin() {
       dispatch(LOGIN(response.user));
       setError(null); // 에러 발생하지 않음
       setIsPending(false); // 통신 종료
+      if (!response.user.profile) navigate("/editprofile", { replace: true });
+      else navigate("/survey");
     } else {
       if (typeof response.message === "string") setError(response.message);
       setIsPending(false); // 통신 종료
