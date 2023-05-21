2a1aee5da538b806618eb914b98810101ef1f602
Skylark92 <skylark92@live.co.kr>
2023-05-22 03:28:15 +0900

    [Style] SignUpInput 컴포넌트 라벨 스타일 추가

--- src/components/SignUp/SignUpInput.jsx
+++ src/components/SignUp/SignUpInput.jsx
@@ -1,10 +1,12 @@
+import "./SignUpInput.css";
+
 function SignUpInput({ id, name, type, label }) {
   return (
     <>
-      <label htmlFor={id}>{label}</label>
+      <label className="sign-up-input-label" htmlFor={id}>{label}</label>
       <input id={id} name={name} type={type} />
     </>
   )
 }
 
-export default SignUpInput;
+export default SignUpInput
